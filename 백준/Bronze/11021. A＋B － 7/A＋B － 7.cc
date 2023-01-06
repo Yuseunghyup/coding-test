4b0cd0042e52b3552eadc423a6d798027f575664
Yuseunghyup <118142664+Yuseunghyup@users.noreply.github.com>
2023-01-06 15:24:17 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+
+    int A, B;
+    
+    for (int i = 1; i <= T; i++)
+    {
+        cin >> A >> B;
+        cout << "Case #" << i << ": " << A + B << endl;
+    }
+
+    return 0;
+}
